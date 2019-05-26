3d1d5f7b836bc9e08d38fcdebb3e3ebd1c027a96
Oniya Daniel <daniel.prince84@gmail.com>
2019-05-26 05:33:37 +0100

    auth scaffolding with logout

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,9 +6,31 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
-    <link rel="stylesheet" href="{{ asset('dist/css/main.css') }}" />
+    <link rel="stylesheet" href="{{ asset('dist/css/main.css') }}"/>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container p-2">
+        <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link btn btn-outline-secondary px-4" href="{{ route('login') }}">Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <div class="container vh-100">
     <div class="row h-100 align-items-center">
         <div class="col-6 text-right">
